6c2d146dad530a3829cee6772bda4cd6b87867bb
NithinSS <nithinsn@protonmail.com>
2020-09-14 16:45:27 +0530

    display selection list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
+import { HEROES } from '../mock-heroes';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
 
   hero: Hero = {
     id: 1,
@@ -16,6 +20,11 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
